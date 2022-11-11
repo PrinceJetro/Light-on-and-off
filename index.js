360530ac21922881427a5dc396c59704ce55099d
PrinceJetro <adegbuyijephthah@gmail.com>
2022-11-11 23:41:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let lighton= "<img src='lighton.png'>";
+let lightoff= "<img src='lightoff.png'>";
+
+function on(){
+    document.getElementById("bulb").innerHTML=lighton
+};
+
+function off(){
+    document.getElementById("bulb").innerHTML=lightoff
+}
